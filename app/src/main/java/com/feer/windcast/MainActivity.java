3e31861512ec0e5163fe22e2ea8f7806fa6b77e7
Reef <reef.turner@gmail.com>
2014-02-08 23:57:34 +0800

    Graph is populated with data from bom
    The graph x axis is the wrong way around
    it is also quite squishy and hard to read.

--- app/src/main/java/com/feer/windcast/MainActivity.java
+++ app/src/main/java/com/feer/windcast/MainActivity.java
@@ -28,7 +28,9 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ListIterator;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -136,55 +138,57 @@ public class MainActivity extends ActionBarActivity {
                             sb.append(reading.LocalTime); sb.append("\n\n");
                             sb.append("Latest Wind Reading:");
 
+                            label.setText(sb.toString());
+
                             if(reading.WindBearing != null && reading.CardinalWindDirection != null && reading.WindSpeed_KMH != null)
                             {
                                 sb.append(reading.WindBearing);
                                 sb.append(" (" +reading.CardinalWindDirection + " ) ");
                                 sb.append(" " + reading.WindSpeed_KMH);
                             }
-                        }
 
-                        label.setText(sb.toString());
+                            ArrayList<Number> windSpeeds = new ArrayList<Number>(wd.ObservationData.size());
+                            ListIterator insertItr = windSpeeds.listIterator();
+                            for(ObservationReading reading1 : wd.ObservationData)
+                            {
+                                Number val = reading1.WindSpeed_KMH != null ?
+                                        reading1.WindSpeed_KMH : 0;
+
+                                insertItr.add(val);
+                            }
+
+
+                            // initialize our XYPlot reference:
+                            plot = (XYPlot) getActivity().findViewById(R.id.mySimpleXYPlot);
+
+                            // Create a couple arrays of y-values to plot:
+                            Number[] series1Numbers = windSpeeds.toArray(new Number[windSpeeds.size()]);
+
+                            // Turn the above arrays into XYSeries':
+                            XYSeries series1 = new SimpleXYSeries(
+                                    Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
+                                    SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
+                                    "Series1");                             // Set the display title of the series
+
+                            // Create a formatter to use for drawing a series using LineAndPointRenderer
+                            // and configure it from xml:
+                            LineAndPointFormatter series1Format = new LineAndPointFormatter();
+                            series1Format.setPointLabelFormatter(new PointLabelFormatter());
+                            series1Format.configure(getActivity().getApplicationContext(),
+                                    R.xml.line_point_formatter_with_plf1);
+
+                            // add a new series' to the xyplot:
+                            plot.addSeries(series1, series1Format);
+
+                            // reduce the number of range labels
+                            plot.setTicksPerRangeLabel(3);
+                            plot.getGraphWidget().setDomainLabelOrientation(-45);
+                        }
                     }
                 }
             }.execute();
 
-            // initialize our XYPlot reference:
-            plot = (XYPlot) getActivity().findViewById(R.id.mySimpleXYPlot);
 
-            // Create a couple arrays of y-values to plot:
-            Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
-            Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
-
-            // Turn the above arrays into XYSeries':
-            XYSeries series1 = new SimpleXYSeries(
-                    Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
-                    SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
-                    "Series1");                             // Set the display title of the series
-
-            // same as above
-            XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
-
-            // Create a formatter to use for drawing a series using LineAndPointRenderer
-            // and configure it from xml:
-            LineAndPointFormatter series1Format = new LineAndPointFormatter();
-            series1Format.setPointLabelFormatter(new PointLabelFormatter());
-            series1Format.configure(getActivity().getApplicationContext(),
-                    R.xml.line_point_formatter_with_plf1);
-
-            // add a new series' to the xyplot:
-            plot.addSeries(series1, series1Format);
-
-            // same as above:
-            LineAndPointFormatter series2Format = new LineAndPointFormatter();
-            series2Format.setPointLabelFormatter(new PointLabelFormatter());
-            series2Format.configure(getActivity().getApplicationContext(),
-                    R.xml.line_point_formatter_with_plf2);
-            plot.addSeries(series2, series2Format);
-
-            // reduce the number of range labels
-            plot.setTicksPerRangeLabel(3);
-            plot.getGraphWidget().setDomainLabelOrientation(-45);
 
         }
     }
