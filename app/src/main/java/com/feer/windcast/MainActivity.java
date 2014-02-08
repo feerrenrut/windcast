ff08f7d6cd4f5f9e87f2c31df1e156ceced5027b
Reef <reef.turner@gmail.com>
2014-02-08 21:20:26 +0800

    shows a populated graph

--- app/src/main/java/com/feer/windcast/MainActivity.java
+++ app/src/main/java/com/feer/windcast/MainActivity.java
@@ -14,6 +14,12 @@ import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.TextView;
 
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PointLabelFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
 import org.apache.http.util.ExceptionUtils;
 
 import java.io.BufferedInputStream;
@@ -22,6 +28,7 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -72,6 +79,8 @@ public class MainActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+
+        private XYPlot plot;
 
         @Override
         public void onResume()
@@ -139,6 +148,44 @@ public class MainActivity extends ActionBarActivity {
                     }
                 }
             }.execute();
+
+            // initialize our XYPlot reference:
+            plot = (XYPlot) getActivity().findViewById(R.id.mySimpleXYPlot);
+
+            // Create a couple arrays of y-values to plot:
+            Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
+            Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
+
+            // Turn the above arrays into XYSeries':
+            XYSeries series1 = new SimpleXYSeries(
+                    Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
+                    SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
+                    "Series1");                             // Set the display title of the series
+
+            // same as above
+            XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+
+            // Create a formatter to use for drawing a series using LineAndPointRenderer
+            // and configure it from xml:
+            LineAndPointFormatter series1Format = new LineAndPointFormatter();
+            series1Format.setPointLabelFormatter(new PointLabelFormatter());
+            series1Format.configure(getActivity().getApplicationContext(),
+                    R.xml.line_point_formatter_with_plf1);
+
+            // add a new series' to the xyplot:
+            plot.addSeries(series1, series1Format);
+
+            // same as above:
+            LineAndPointFormatter series2Format = new LineAndPointFormatter();
+            series2Format.setPointLabelFormatter(new PointLabelFormatter());
+            series2Format.configure(getActivity().getApplicationContext(),
+                    R.xml.line_point_formatter_with_plf2);
+            plot.addSeries(series2, series2Format);
+
+            // reduce the number of range labels
+            plot.setTicksPerRangeLabel(3);
+            plot.getGraphWidget().setDomainLabelOrientation(-45);
+
         }
     }
 
