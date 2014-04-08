68baf7ccaea83e3b1e7e209643ec8cd2a5edc4a1
Reef <reef.turner@gmail.com>
2014-04-08 21:14:46 +0800

    extended tests
    Added an item matcher, to match against hint text.
    The hint text matcher is then used against the search input to verify that its initial text is correct.
    An example of how to check the contents of the weather station list has been added.

--- app/src/androidTest/java/com/feer/windcast/tests/testStationFragment.java
+++ app/src/androidTest/java/com/feer/windcast/tests/testStationFragment.java
@@ -1,20 +1,29 @@
 package com.feer.windcast.tests;
 
 import android.test.ActivityInstrumentationTestCase2;
-import android.widget.EditText;
 
 import com.feer.windcast.MainActivity;
 import com.feer.windcast.R;
+import com.feer.windcast.WeatherStation;
+import com.google.android.apps.common.testing.ui.espresso.ViewInteraction;
 
-import static android.test.ViewAsserts.assertOnScreen;
+import static com.feer.windcast.testUtils.ItemHintMatchers.withItemHint;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
 
 /**
  * http://youtu.be/uHoB0KzQGRg?t=54s
+ * see https://code.google.com/p/android-test-kit/wiki/EspressoStartGuide
  */
 public class testStationFragment extends ActivityInstrumentationTestCase2<MainActivity>
 {
     private MainActivity mMainActivity;
-    private EditText mSearchInput;
 
     private static final String PACKAGE_TO_TEST = "com.feer.windcast";
 
@@ -30,12 +39,26 @@ public class testStationFragment extends ActivityInstrumentationTestCase2<MainAc
         super.setUp();
 
         mMainActivity = getActivity();
-        mSearchInput = (EditText) mMainActivity.findViewById(R.id.weather_station_search_box);
     }
 
     public void testSearchBoxOnScreen()
     {
-        assertOnScreen(mMainActivity.getWindow().getDecorView(), mSearchInput);
+        ViewInteraction searchBox = onView(withId(R.id.weather_station_search_box));
+        searchBox.check(matches(isDisplayed()));
+        searchBox.check(matches(withItemHint(R.string.weather_station_search_text)));
+    }
+
+    public void testWeatherStationItemsExit()
+    {
+        onData(instanceOf(WeatherStation.class))
+            .inAdapterView(allOf(withId(android.R.id.list), isDisplayed()))
+            .atPosition(15)
+            .check(matches(isDisplayed()));
+
+        onData(instanceOf(WeatherStation.class))
+                .inAdapterView(withId(android.R.id.list))
+                .atPosition(2)
+                .check(matches(withText("Adele Island (WA)")));
     }
 
 
